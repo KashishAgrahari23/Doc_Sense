b1849456662debf098b57e24116ebadec1af5668
KashishAgrahari23 <kashishagrahari23@gmail.com>
2026-01-17 11:29:38 +0530

    text extraction done

--- src/lib/extractText.js
+++ src/lib/extractText.js
@@ -1,9 +1,10 @@
-import pdfParse from "pdf-parse";
 import mammoth from "mammoth";
 
 export async function extractTextFromFile(buffer, mimeType) {
   // PDF
   if (mimeType === "application/pdf") {
+    // 👇 dynamic require (IMPORTANT)
+    const pdfParse = (await import("pdf-parse")).default;
     const data = await pdfParse(buffer);
     return data.text;
   }
